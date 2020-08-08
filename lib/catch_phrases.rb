933c7e21b8eb6d1537e24573600c1c501aa2632b
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-08 23:16:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -13,3 +13,8 @@ def link
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
